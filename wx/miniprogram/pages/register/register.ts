bdee117d5650411771d5dba68877c811f52e459f
codefan <1993036922@qq.com>
2024-01-30 16:05:06 +0800

    上传驾照

---
+++ wx/miniprogram/pages/register/register.ts
@@ -0,0 +1,77 @@
+Page({
+  data: {
+    name: '',
+    licNo: '',
+    licImgUrl: '',
+    birthDate: '',
+    genderIndex: 0,
+    hasLicImg: false,
+    state: 'UNSUBMITTED',
+    genders: ['未知', '男', '女', '其他'],
+  },
+  // 上传驾照
+  onUploadLic() {
+    wx.chooseMedia({
+      success: (res) => {
+        if (res.tempFiles.length > 0) {
+          this.setData({
+            licImgUrl: res.tempFiles[0].tempFilePath,
+            hasLicImg: true
+          }),
+            setTimeout(() => {
+              this.setData({
+                name: '张三',
+                genderIndex: 1,
+                licNo: '15975121689',
+                birthDate: '1990-12-01'
+              })
+            }, 1000)
+        }
+      },
+      fail: console.error
+    })
+  },
+  // 修改性别
+  onGenderChange(e: any) {
+    this.setData({
+      genderIndex: e.detail.value
+    })
+  },
+  // 修改生日
+  onBirthDateChange(e: any) {
+    this.setData({
+      birthDate: e.detail.value
+    })
+  },
+  // 提交审查
+  onSubmit() {
+    this.setData({
+      state: 'PENDING'
+    })
+    setTimeout(() => {
+      this.onLicVerified()
+    }, 3000)
+    
+  },
+  // 重新审查
+  onResubmit() {
+    this.setData({
+      name: '',
+      licNo: '',
+      birthDate: '',
+      genderIndex: 0,
+      hasLicImg: false,
+      state: 'UNSUBMITTED',
+      licImgUrl: undefined,
+    })
+  },
+  // 前往开锁页面
+  onLicVerified() {
+    this.setData({
+      state: 'VERIFIED'
+    })
+    wx.redirectTo({
+      url: '/pages/lock/lock'
+    })
+  }
+})

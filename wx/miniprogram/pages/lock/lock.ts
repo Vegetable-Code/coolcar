078919f2bff774b6e9756be559175284809de123
codefan <1993036922@qq.com>
2024-02-01 09:44:04 +0800

    开锁页面

--- wx/miniprogram/pages/lock/lock.ts
+++ wx/miniprogram/pages/lock/lock.ts
@@ -14,6 +14,7 @@ Page({
       shareLocation: wx.getStorageSync(shareLocationKey) || false
     })
   },
+  // 获取用户头像
   onGetUserInfo(e: any) {
     console.log(e)
     const userInfo: WechatMiniprogram.UserInfo = e.detail.userInfo
@@ -21,9 +22,24 @@ Page({
       getApp<IAppOption>().resolveUserInfo(userInfo)
     }
   },
+  // 是否共享头像
   onShareLocation(e: any) {
     const shareLocation: boolean = e.detail.value
     wx.setStorageSync('share_location', shareLocation)
   },
-  onUnlockTap() { },
+  // 开锁
+  onUnlockTap() {
+    wx.showLoading({
+      title: '开锁中...',
+      mask: true
+    })
+    setTimeout(() => {
+      wx.redirectTo({
+        url: '/pages/driving/driving',
+        complete: () => {
+          wx.hideLoading()
+        }
+      })
+    }, 2000);
+  },
 })
